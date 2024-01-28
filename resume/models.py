6a47c5497b80466cdef2db6740d0382e25e0bd61
Shokhjahon <112770922+shokhjahon-dev@users.noreply.github.com>
2024-01-28 21:06:27 +0500

    Update models.py 2

--- resume/models.py
+++ resume/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
 class Education(models.Model):
     years = models.CharField(max_length=20, verbose_name="Yili")
     topic = models.CharField(max_length=255, verbose_name="Mavzusi")
